67ea13ffd68478e892284051ebcc5565fe669e54
Cody Hess <>
2017-08-05 17:29:41 -0400

    giraffe can kiss and poop

--- hearts-v1.js
+++ hearts-v1.js
@@ -5,41 +5,37 @@ g.right = true;
 g.pre = undefined;
 g.str = '';
 g.kisses = [];
-g.poops = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.addEventListener('touchstart', (event) => {
-    if (g.right) g.kisses.unshift([g.x, 0]);
-    if (!g.right) g.poops.unshift([g.x, 0]);
-  });
   document.addEventListener('keypress', (event) => {
-    if (['k','K','p','P'].includes(event.key)) {
-      if (g.right) g.kisses.unshift([g.x, 0]);
-      if (!g.right) g.poops.unshift([g.x, 0]);
+    if (event.key === 'k') {
+      g.kisses.push([g.x, g.right]);
     }
   });
   g.pre = document.getElementById('animation');
   window.setInterval( () => {
     g.str = '\n';
     // draw the giraffe head
+    var headArr = Array(32);
+    for (var i=0; i<headArr.length; i++) {
+      headArr[i] = ' ';
+    }
+    for (var i=0; i<g.kisses.length; i++) {
+      if (g.kisses[i][1]) {
+        headArr[g.kisses[i][0]] = '<';
+        headArr[g.kisses[i][0]+1] = '3';
+      } else {
+        headArr[g.kisses[i][0]] = 'є';
+        headArr[g.kisses[i][0]-1] = '<';
+      }
+    }
+    /*
     if (g.right) {
       g.str += ' '.repeat(g.x+2);
       if (g.open) {
         g.str += '^^=<';
       } else {
         g.str += '^^==';
-      }
-      // draw the kisses
-      var curX = g.x+6;
-      for (var i=0; i<g.kisses.length; i++) {
-        var bump = g.kisses[i][1];
-        if (i>0) bump = bump - g.kisses[i-1][1];
-        if (bump>0) g.str += ' '.repeat(bump);
-        if (g.kisses[i][1]) {
-          g.str += '<3';
-        } else {
-          g.str += '3';
-        }
       }
     } else {
       g.str += ' '.repeat(g.x);
@@ -49,6 +45,8 @@ document.addEventListener('DOMContentLoaded', () => {
         g.str += '==^^';
       }
     }
+    */
+    g.str += headArr.join('');
     g.str += '\n';
     
     // draw the giraffe neck
@@ -62,11 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // draw the giraffe body
     if (!g.right) g.str += ' ';
     g.str += ' '.repeat(g.x);
-    g.str += '[===]';
-    for (var i=0; i<g.poops.length; i++) {
-      if (g.poops[i][1] === 0) g.str += '3';
-    }
-    g.str += '\n';
+    g.str += '[===]\n';
 
     // draw the giraffe feet
     if (!g.right) g.str += ' ';
@@ -77,16 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
       g.str += '/| /|';
     } else {
       g.str += '|| ||';
-    }
-    if (!g.right) {
-      var curX = g.x+5;
-      for (var i=0; i<g.poops.length; i++) {
-        var bump = g.poops[i][0]+5-curX;
-        curX = curX + bump;
-        //if (i>0) bump = bump - g.kisses[i-1][1];
-        if (bump>0) g.str += ' '.repeat(bump);
-        if (g.poops[i][1] < 0) g.str += '<3';
-      }
     }
 
     // draw the element
@@ -102,19 +86,18 @@ document.addEventListener('DOMContentLoaded', () => {
     // scoot the giraffe
     g.x = g.right ? g.x + 1 : g.x - 1;
 
-    // scoot the kisses and poops
-    g.kisses = g.kisses.filter((k) => {
-      return ( (k[0]+k[1]) < 32 );
-    });
+    // scoot the kisses
     for (var i=0; i<g.kisses.length; i++) {
-      g.kisses[i][1] += 2;
+      if (g.kisses[i][1]) {
+        g.kisses[i][0] += 2;
+      } else {
+        g.kisses[i][0] -= 2;
+      }
     }
-    g.poops = g.poops.filter((k) => {
-      return k[1] > -4;
-    });
-    for (var i=0; i<g.poops.length; i++) {
-      g.poops[i][1] -= 1;
-    }
+    g.kisses = g.kisses.filter((x) => {
+      return (x[0] >0 && x[0] <32)
+    })
+    if (g.kisses.length > 0) console.log(g.kisses);
 
   }, 150)
 })
